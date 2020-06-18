47d17f9998a4a01a9fea1c9af93c4ef21aa738d9
Thomas Raout <thomas.raout-ext@pernod-ricard.com>
2020-06-18 15:12:25 +0200

    init nuxt

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1> default {{name}}</h1>
+</template>
+
+<script>
+
+export default {
+    name: "test",
+    middleware ({ redirect }) {
+        return redirect('/login')
+        // If the user is not authenticated
+        // if (!store.state.authenticated) {
+        //   return redirect('/login')
+        // }
+  }
+}
+
+</script>
+
+<style>
+</style>
