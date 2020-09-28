5c9d824db8b712b0bd90f93d4762b9f5af8a3742
Thomas Raout <thomraout@gmail.com>
2020-09-28 18:07:31 +0200

    add middleware

---
+++ middleware/adminAuthenticated.ts
@@ -0,0 +1,30 @@
+// If the user is not authenticated
+export default function ({ app, store, redirect }) {
+    const token = store.getters.getToken;
+    if (!token) {
+        const cookieToken = app.$cookies.get('token')
+
+        // If the token is not present in the store, but is present in the cookie
+        // It happens if the user closes the window and reopens it for example
+        if (!cookieToken) {
+            store.commit("logout")
+            return redirect("/login")
+        } else {
+
+            const payload = cookieToken.split(".")[1]
+            const decodedPayload = Buffer.from(payload, 'base64').toString()
+            const jsonPayload = JSON.parse(decodedPayload)
+
+            if (!jsonPayload.isAdmin) {
+                store.commit("logout")
+                return redirect("/login")
+            }
+
+            store.commit("login", {
+                userId: jsonPayload.id,
+                isAdmin: jsonPayload.isAdmin,
+                token: cookieToken
+            })
+        }
+    }
+}
---
+++ pages/users.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ users }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    const req = await context.app.$axios.get(`/list/users`, {});
+    return {
+      users: req.data
+    };
+  },
+  middleware: "adminAuthenticated",
+  methods: {}
+};
+</script>
+
+<style></style>
