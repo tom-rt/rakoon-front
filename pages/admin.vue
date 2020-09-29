8bb27d6635a318360e4b5e9bc70810a33bfc0fd9
Thomas Raout <thomraout@gmail.com>
2020-09-29 20:10:56 +0200

    admin panel in progress

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,69 +1,101 @@
 <template>
   <div class="flex flex-row w-full h-full flex-wrap p-4">
-    <div class="text-3xl font-extrabold">Utilisateurs</div>
-    <!-- {{ users }} -->
+    <div class="text-3xl font-extrabold pb-4">Utilisateurs</div>
     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
       <table class="min-w-full divide-y divide-gray-200">
-        <thead>
+        <thead class="bg-gray-700">
           <tr>
             <th
-              class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-100 uppercase tracking-wider"
             >
               Id
             </th>
             <th
-              class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-100 uppercase tracking-wider"
             >
-              Name
+              Nom
             </th>
             <th
-              class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-100 uppercase tracking-wider"
             >
               Reauth
             </th>
             <th
-              class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-100 uppercase tracking-wider"
             >
-              Cree le
+              Crée le
             </th>
             <th
-              class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-100 uppercase tracking-wider"
             >
-              Derniere connexion
+              Dernière connexion
             </th>
             <th
-              class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-100 uppercase tracking-wider"
             >
-              Archive le
+              Archivé le
             </th>
             <th
-              class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-100 uppercase tracking-wider"
             >
               Admin
             </th>
           </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
-          <tr v-for="user in users" :key="user">
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+          <tr
+            v-bind:class="{ 'bg-gray-300': users.indexOf(user) % 2 == 1 }"
+            v-for="(user, idx) in users"
+            :key="idx"
+          >
+            <td
+              class="px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
               {{ user.id }}
             </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+            <td
+              class="px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
               {{ user.name }}
             </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+            <td
+              v-bind:class="{
+                'text-green-500': user.reauth,
+                'text-red-500': !user.reauth
+              }"
+              class="font-extrabold px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
               {{ user.reauth }}
             </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+            <td
+              class="px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
               {{ user.created_on }}
             </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+            <td
+              class="px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
               {{ user.last_login }}
             </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-              {{ user.archived_on }}
+            <td
+              v-if="user.archived_on.Valid"
+              class="px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
+              {{ user.archived_on.Time }}
             </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+            <td
+              v-if="!user.archived_on.Valid"
+              class="px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
+              null
+            </td>
+            <td
+              v-bind:class="{
+                'text-green-500': user.is_admin,
+                'text-red-500': !user.is_admin
+              }"
+              class="font-extrabold px-6 py-4 whitespace-no-wrap text-center text-sm leading-5"
+            >
               {{ user.is_admin }}
             </td>
           </tr>
@@ -74,9 +106,23 @@
 </template>
 
 <script>
+var dayjs = require("dayjs");
+
 export default {
   async asyncData(context) {
     const req = await context.app.$axios.get(`/list/users`, {});
+    for (let i = 0; i < req.data.length; i++) {
+      req.data[i].created_on = dayjs(req.data[i].created_on).format(
+        "DD/MM/YYYY HH:mm"
+      );
+      req.data[i].last_login = dayjs(req.data[i].last_login).format(
+        "DD/MM/YYYY HH:mm"
+      );
+      req.data[i].archived_on.Time = dayjs(req.data[i].archived_on.Time).format(
+        "DD/MM/YYYY HH:mm"
+      );
+    }
+    console.log(req.data);
     return {
       users: req.data
     };
