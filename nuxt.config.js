3579dde5af6bba2d3693eff605d3784902aa1cfa
Thomas Raout <thomraout@gmail.com>
2020-10-31 16:22:42 +0100

    base url set to dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,8 @@
 export default {
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/axios", "cookie-universal-nuxt"],
   axios: {
-    baseURL: "http://localhost:8081/v1",
+    // baseURL: "http://localhost:8081/v1",
+    baseURL: "5.135.165.57:8081/v1",
   },
   plugins: ["~/plugins/axios.ts", "~/plugins/click-outside.ts"],
   css: [{ src: "~assets/css/style.css", lang: "css" }],
