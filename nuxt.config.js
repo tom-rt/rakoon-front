ff072c92e4c9183b9bfb02324324b61d786a4983
Thomas Raout <thomraout@gmail.com>
2020-10-31 19:55:17 +0100

    url set correctly

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,7 @@ export default {
   target: 'static',
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/axios", "cookie-universal-nuxt"],
   axios: {
-    // baseURL: "http://localhost:8081/v1",
-    baseURL: "5.135.165.57:8081/v1",
+    baseURL: "http://5.135.165.57:8081/v1",
   },
   plugins: ["~/plugins/axios.ts", "~/plugins/click-outside.ts"],
   css: [{ src: "~assets/css/style.css", lang: "css" }],
