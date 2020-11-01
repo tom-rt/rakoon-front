94453aed90527cef0482220fb9bf0b76c81559c0
Thomas Raout <thomraout@gmail.com>
2020-11-01 15:19:39 +0100

    add redirections

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,9 +6,6 @@ export default {
   axios: {
     baseURL: "http://5.135.165.57:8081/v1",
   },
-  generate: {
-    fallback: true
-  },
   plugins: ["~/plugins/axios.ts", "~/plugins/click-outside.ts"],
   css: [{ src: "~assets/css/style.css", lang: "css" }],
   buildDir: 'dist',
