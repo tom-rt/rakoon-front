3d092b593c9fb736847a1f42cc5abbc4afa5fe63
Thomas Raout <thomraout@gmail.com>
2020-11-08 15:15:49 +0100

    prod url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 export default {
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/axios", "cookie-universal-nuxt"],
   axios: {
-    // baseURL: "https://api.rakoon.tech:443/v1",
-    baseURL: "http://localhost:8081/v1",
+    baseURL: "https://api.rakoon.tech:443/v1",
+    // baseURL: "http://localhost:8081/v1",
   },
   plugins: ["~/plugins/axios.ts", "~/plugins/click-outside.ts"],
   css: [{ src: "~assets/css/style.css", lang: "css" }],
