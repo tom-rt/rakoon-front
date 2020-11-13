4f6f72ed7bfbe1cbe4b188c660d9ebf45bc925c7
Thomas Raout <thomraout@gmail.com>
2020-11-13 10:37:48 +0100

    redirect ssl

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,12 @@
+import redirectSSL from 'redirect-ssl'
+
 export default {
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/axios", "cookie-universal-nuxt"],
+  serverMiddleware: [
+    redirectSSL.create({
+      enabled: process.env.NODE_ENV === 'production'
+    }),
+  ],
   axios: {
     baseURL: "https://api.rakoon.tech:443/v1",
     // baseURL: "http://localhost:8081/v1",
