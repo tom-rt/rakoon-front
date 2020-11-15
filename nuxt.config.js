a92908454d2731ab7c7b3c97666a529504ed6a46
Thomas Raout <thomraout@gmail.com>
2020-11-15 20:07:53 +0100

    prod url

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,8 @@ export default {
     }),
   ],
   axios: {
-    // baseURL: "https://api.rakoon.tech:443/v1",
-    baseURL: "http://localhost:8081/v1",
+    baseURL: "https://api.rakoon.tech:443/v1",
+    // baseURL: "http://localhost:8081/v1",
   },
   plugins: ["~/plugins/axios.ts", "~/plugins/click-outside.ts"],
   css: [{ src: "~assets/css/style.css", lang: "css" }],
